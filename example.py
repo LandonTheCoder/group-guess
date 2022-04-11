88f7efab5ba8f541ab520fa0959535001c0a24c4
LandonTheCoder <100165458+LandonTheCoder@users.noreply.github.com>
2022-04-11 15:56:32 -0400

    Add preliminary example code This serves to show how the library is meant to work, as a brief introduction. I didn't do anything fancy, like CLI arguments, for time's sake.

---
+++ example.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+"""Very basic example code.
+
+Meant to be used as a guide for new games, until a savefile is implemented."""
+
+import group_guess
+
+class ExampleWindow(group_guess.AppWindow):
+  def create_questions(self):
+    """Create game questions."""
+    # group_guess.Answer(displayname, alts=(), ppl=None)
+    example_question_answers = [group_guess.Answer("red", ppl=30),
+                                group_guess.Answer("blue", ppl=25),
+                                group_guess.Answer("yellow", ppl=20)]
+    # group_guess.Question(question, the_id, answers)
+    example_question_1 = group_guess.Question("What is your favorite color?",
+                                              "favorite-color",
+                                              example_question_answers)
+    # Need to define self.questions
+    self.questions = [example_question_1]
+  # You can define do_subclass_make_widgets() here, although it's optional.
+
+def main():
+  test_window = ExampleWindow(debug=True)
+  gtk.main()
