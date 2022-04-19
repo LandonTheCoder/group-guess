517a68842505d12644dca9be2e9c4e50024ab6ab
LandonTheCoder <100165458+LandonTheCoder@users.noreply.github.com>
2022-04-19 15:22:59 -0400

    Fix uninstalled module detection bug I discovered that if group_guess/example.py is executed when the group_guess module isn't installed per-user or systemwide, it won't find the group_guess module. Since it searches the script directory (a.k.a. the group_guess directory) first, I made it try importing __init__ as group_guess if the group_guess module isn't found.
    This should help with the experience of using Group Guess uninstalled.

--- group_guess/example.py
+++ group_guess/example.py
@@ -3,7 +3,19 @@
 
 Meant to be used as a guide for new games, until a savefile is implemented."""
 
-import group_guess
+try:
+  # This will work if the group_guess module is installed,
+  # as a directory group_guess in sys.path
+  import group_guess
+except ModuleNotFoundError:
+  # If group_guess isn't installed systemwide or per-user, this will import,
+  # as it won't look for the module, just a Python source file.
+  # Since loading a module group_guess is equivalent to loading __init__
+  # within its module folder, and this is within said folder, this just
+  # loads the __init__ within group_guess, which is the script folder,
+  # manually.
+  import __init__ as group_guess
+
 import gi; gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk as gtk
 
